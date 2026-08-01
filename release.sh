b8b82b6a83a51e5facd85bb3ec4cb5327e7a41b1
Jean-François Lamy <jf@jflamy.dev>
2026-08-01 17:17:12 -0400

    Release 3.0.0-alpha01

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-TAG="${TAG:-${1:-3.0.0-alpha01}"
+TAG="${TAG:-${1:-3.0.0-alpha01}}"
 set -euo pipefail
 
 # Triggers the GitHub Actions workflow `.github/workflows/release.yaml`
